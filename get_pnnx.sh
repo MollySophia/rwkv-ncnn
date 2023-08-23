92538cde05c03169f6dea4dcc62c44efac8ac773
Molly Sophia <mollysophia379@gmail.com>
2023-08-23 20:06:47 +0800

    Remove get_pnnx.sh
    Signed-off-by: Molly Sophia <mollysophia379@gmail.com>

--- get_pnnx.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-__version="20230816"
-
-wget https://github.com/pnnx/pnnx/releases/download/${__version}/pnnx-${__version}-ubuntu.zip \
-    && unzip pnnx-${__version}-ubuntu.zip && mv pnnx-${__version}-ubuntu/pnnx . \
-    && rm -r pnnx-${__version}-ubuntu pnnx-${__version}-ubuntu.zip
